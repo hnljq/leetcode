9e32d69edb13eaca24ac4b762de366f9d9fab544
itst <itst@qq.com>
2023-10-04 17:00:32 +0800

    init

--- test/sum.py
+++ test/sum.py
@@ -20,17 +20,17 @@ def num(x:List):
 
 
 def max(x: List):
-	if len(x) == 0:
-		return None
-	elif len(x) == 1:
-		return x[0]
-	elif len(x) == 2:
-		if x[0]>= x[1]:
-			return x[0]
-		else:
-			return x[1]
-	else:
-		
+    if len(x) == 0:
+        return None
+    elif len(x) == 1:
+        return x[0]
+    elif len(x) == 2:
+        if x[0]>= x[1]:
+            return x[0]
+        else:
+            return x[1]
+    else:
+
 
 
 if __name__ == '__main__':
